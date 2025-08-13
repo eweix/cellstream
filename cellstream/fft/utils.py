a7c914ae1bd444e7d7ce8c28204ca193783f990f
eweix <ewweix@wisc.edu>
2025-08-13 12:02:37 -0500

    feat: fft angle support for devices without .angle() implementation
    cellstream.fft.utils.generate_fft_features() includes a parameter for
    device (i.e. a pytorch-supported device, such as "cuda" or "mps").
    However, "mps" does not support the necessary complex angle
    calculations; attempting such a calculation returns a NotImplemented
    error.
    
    To take advantage of speed gains from gpu devices that do not support
    complex angle calculations, a NotImplemented exception has been added
    to:
    - cellstream.fft.utils.generate_fft_features()
    
    The same fix does not work in cellstream.cwt.utils.query_cwt_block()
    because ssqueezepy only supports cuda acceleration.

--- cellstream/fft/utils.py
+++ cellstream/fft/utils.py
@@ -122,7 +122,11 @@ def generate_fft_features(
                 buffers["z_score"][:, :, start:end] = z[:max_bin].cpu()
 
             if "phase" in fft_features_to_process:
-                phase = fft_chunk.angle()
+                try:
+                    phase = fft_chunk.angle()
+                except NotImplemented:  # process on cpu if gpu can't do it
+                    phase = fft_chunk.cpu().angle()
+                    phase = phase.to("cpu" if device is None else device)
                 buffers["phase"][:, :, start:end] = phase[:max_bin].cpu()
 
             bar.update(end)
@@ -146,7 +150,11 @@ def generate_fft_features(
             feature_map["z_score"] = z[:max_bin]
 
         if "phase" in fft_features_to_process:
-            phase = fft.angle()
+            try:
+                phase = fft.angle()
+            except NotImplemented:
+                phase = fft.cpu().angle()
+                phase = phase.to("cpu" if device is None else device)
             feature_map["phase"] = phase[:max_bin]
 
     return feature_map
